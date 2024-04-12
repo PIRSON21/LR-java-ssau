3091faf67be07e76fa8b717a1263c34503493da5
Egor Kochetkov <pirs2132@gmail.com>
2024-04-12 08:03:59 +0400

    Added answers.md and README.md

--- 5/LAB_5/src/Int.java
+++
@@ -1,9 +0,0 @@
- interface Int {
-    int getElement(int n);
-    void setElement(int n, int k);
-    String getName();
-    int getIntroduce();
-    int calcPages() throws PagesLessZeroException;
-    boolean equals(Object o);
-    int getLen();
-}
--- 5/LAB_5/src/PagesLessZeroException.java
+++
@@ -1,5 +0,0 @@
-public class PagesLessZeroException extends Exception {
-    public PagesLessZeroException(String message) {
-        super(message);
-    }
-}
